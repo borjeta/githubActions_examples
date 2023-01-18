ee94f4b474c6ba585a7beb116e4c0d8f568eaf2a
borjeta <borjaalventosa@gmail.com>
2023-01-18 17:23:51 +0100

    commit con fallos y el action

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /* Crear un servidor HTTP básico por el puerto 3000 */
-const http = require('http');
+const http = require('httap');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('Hello World');d adsasdda asd
 }
-);
+);f
 server.listen(3000, () => {
     console.log('Server running at port 3000');
-    
+    fasdfd
 }
 );
 
