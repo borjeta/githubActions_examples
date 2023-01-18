33a4373ef393d97b7310f18c6fe726c4be1b16cf
borjeta <borjaalventosa@gmail.com>
2023-01-18 19:02:27 +0100

    codigo reparado

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 /* Crear un servidor HTTP básico por el puerto 3000 */
-const http = require('http');
+const http = require("http");
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 }
 );
 server.listen(3000, () => {
-    console.log('Server running at port 3000');
-}
-);
+  console.log("Server running at port 3000");
+});
+
+// Path: server.js
