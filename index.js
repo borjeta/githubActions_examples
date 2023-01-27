a3661022121ce67f3a2d2a7b49727e13fad22d18
Borja Alventosa <borjaalventosa@gmail.com>
2023-01-27 22:40:01 +0100

    arreglando git

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const TOKEN = require()
+// main.js que utilizando Nodejs (la librería node-telegram-bot-api) enviará un mensaje a 722271252
 
+// Importamos la librería node-telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(secrets.TOKEN, { polling: true });
 
-const CHAT_ID = secrets.CHAT_ID;
+// hacemos las peticiones para obtener los tokens de github
+const core = require('@actions/core');
+const { exit } = require('process');
 
-console.log('Bot server started in the port 3000');
+//Cogemos los secrets Telegram_Token y el Telegram_ChatID de las variables de entorno definidas en github
 
-bot.sendMessage(CHAT_ID, 'Hola, soy un bot');
-bot.sendMessage(CHAT_ID, 'Workflow ejecutado correctamente tras el último commit. Saludos '.NOMBRE);
+const TOKEN = core.getInput('TOKEN');
+const CHAT_ID = 1198034886;
+const NOMBRE = core.getInput('NOMBRE');
 
-console.log('Mensaje enviado');
+// Creamos un bot que utiliza 'polling' para obtener actualizaciones
+const bot = new TelegramBot(TOKEN, { polling: true });
+//Le ponemos nombre y lo ponemos en escucha de mensajes
+async function enviamensaja(nombre) {
+    // Enviamos el mensaje
+    await bot.sendMessage(1198034886, `Hola ${nombre}!`);
+    await bot.stopPolling();
+    await process.exit(0);
+}
 
-//prueba
+enviamensaja(NOMBRE);
