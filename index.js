b28acc1433f3716321778e8e67dce498ebf2fb6f
undefined <>
2023-01-28 16:17:17 +0100

    subiendocosas

--- index.js
+++ index.js
@@ -3,45 +3,29 @@ const { exec } = require('child_process');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
-/*importamos la libreria para obtener un meme al azar */
 const { meme } = require('memejs');
-
-
-
-
-/*Obtenemos el nombre del repositorio , el token de github */
 const repo = core.getInput('repo');
 const token = core.getInput('token');
-/*Hacemos una peticion a la api de memejs para obtener un meme al azar */
 
-            
+// programa para modificar el readme cada vez que se haga un push
 
-    /* hacemos un fetch a la api de memejs para obtener un meme al azar 
-    fetch('https://meme-api.herokuapp.com/gimme',{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    
-    })
-        .then(response => response.json())
-        .then(data => {
-            /* obtenemos la url del meme
-            const meme = data.url;
-        });
-        */
-    /* editamos nuestro readme */
+/* Cogemos el readme.md y lo guardamos en una variable */
+const readme = fs.readFileSync('readme.md', 'utf8');
 
-    fs.readFile('readme.md', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        /* obtenemos el meme y lo añadimos al readme */
-        const result = data.replace(/(https:\/\/i\.imgur\.com\/[a-zA-Z0-9]+\.jpg)/g, meme);
-        fs.writeFile('readme.md', result, 'utf8', function (err) {
-            if (err) return console.log(err);
-        });
-    });
+/* Cogemos el numero de commits que hay en el repositorio */
+const commits = exec('git rev-list --count HEAD', (err, stdout, stderr) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(stdout);
+});
+
+/* y actualizamos el readme */
+const newReadme = readme.replace(/commits: \d+/, `commits: ${commits}`);
+
+/* guardamos el readme */
+fs.writeFileSync('readme.md', newReadme);
 
 
     /* hacemos un commit y un push al repositorio */
